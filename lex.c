6591d830b3c8329f8919cf91a5d57fde7e908103
Diego Nieto Cid <diego@myhost.localdomain>
2010-07-20 14:09:19 -0300

    close_include returns a success indicator instead of exitting. yywrap terminates the scanner when close_include fails.

--- lex.c
+++ lex.c
@@ -3364,8 +3364,10 @@ void yyfree (void * ptr )
 	int
 	yywrap (void)
 	 {
-	   close_include ();
-	   return 0;
+           if (close_include () == 0)
+             return 0;
+           else
+             return 1;
 	 }
 
 	#define	MAX_INCLUDE_DEPTH	50
@@ -3404,15 +3406,14 @@ void yyfree (void * ptr )
   	  yy_switch_to_buffer (buffer);
 	}
 
-	/* Close an includefile.  */
-	void
+	/* Close an includefile. returns 0 on success */
+	int
 	close_include (void)
 	{
 	  if ( --include_stack_ptr < 0 )
 	    {
-	    //	      yyterminate ();
 	      fprintf (stderr, "Unexpected end of file at %s:%d.\n", filename, lineno);
-	      exit (1);
+	      return (1);
 	    }
 	  else
 	    {
@@ -3422,6 +3423,7 @@ void yyfree (void * ptr )
 	      lineno = include_stack[include_stack_ptr].currline;
 	      filename = include_stack[include_stack_ptr].filename;
 	      yy_switch_to_buffer (include_stack[include_stack_ptr].buffer);
+              return (0);
 	    }
 	}
 
